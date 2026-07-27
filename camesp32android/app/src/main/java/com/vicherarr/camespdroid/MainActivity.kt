b539ef43c865f82b0e0fe972685018d86c87bdae
victor <vicherarr@gmail.com>
2026-07-27 13:18:34 +0200

    Fix Android App falsely reporting WiFi disconnect by limiting Coil concurrent connections

--- camesp32android/app/src/main/java/com/vicherarr/camespdroid/MainActivity.kt
+++ camesp32android/app/src/main/java/com/vicherarr/camespdroid/MainActivity.kt
@@ -8,10 +8,28 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.vicherarr.camespdroid.ui.MainScreen
 import com.vicherarr.camespdroid.ui.theme.CamEspDroidTheme
 import com.vicherarr.camespdroid.viewmodel.MainViewModel
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import okhttp3.Dispatcher
+import okhttp3.OkHttpClient
+
+import coil.Coil
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        val imageLoader = ImageLoader.Builder(this)
+            .okHttpClient {
+                val dispatcher = Dispatcher().apply {
+                    maxRequestsPerHost = 1
+                }
+                OkHttpClient.Builder()
+                    .dispatcher(dispatcher)
+                    .build()
+            }
+            .build()
+        Coil.setImageLoader(imageLoader)
         enableEdgeToEdge()
         setContent {
             CamEspDroidTheme {
